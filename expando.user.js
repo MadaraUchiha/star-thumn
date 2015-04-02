8a47b3dc21083151341c5cb3e58acce4eb822f9f
Madara Uchiha <me@madara.ninja>
2015-04-02 11:17:59 +0300

    Version bump, 0.2

--- expando.user.js
+++ expando.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Star Thumbnail Expando
 // @resource     STYLE  https://rawgit.com/MadaraUchiha/star-thumn/master/style.css
-// @version      0.1
+// @version      0.2
 // @match        *://chat.stackexchange.com/*
 // @match        *://chat.stackoverflow.com/*
 // @match        *://chat.meta.stackexchange.com/*
